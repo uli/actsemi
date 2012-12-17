2598676e4f6b2779aedde996808b6a16d349b317
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-12-17 09:34:39 +0100

    more syscalls and some prototypes

--- libactsemi/actsemi.h
+++ libactsemi/actsemi.h
@@ -1,9 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+
 int open(const char *pathname, int flags, ...);
 int write(int fd, const void *buf, unsigned int count);
 int close(int fd);
 int read(int fd, void *buf, unsigned int count);
 int remove(const char *pathname);
 int stat(const char *pathname, void *buf);
+
+int rename(const char *oldpath, const char *newpath);
+int mkdir(const char *pathname, uint32_t mode);
+
+void *malloc(uint32_t size);
+void free(void *ptr);
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+#define S_IREAD 0400
+#define S_IWRITE 0200
+#define S_IEXEC 0100
+
+struct dirent {
+  uint8_t _unknown_00[0xe];
+  uint8_t d_type;
+  uint8_t _unknown_0f;
+  char d_name[];
+};
+#define DT_DIR 0x10
+
+typedef void DIR;
+DIR *opendir(const char *name);
+struct dirent *readdir(DIR *);
+int closedir(DIR *);
+
+//typedef void FILE;
+FILE *fopen(const char *path, const char *mode);
+int fclose(FILE *);
+int fseek(FILE *, long offset, int whence);
+long ftell(FILE *);
+void setbuf(FILE *stream, char *buf);
+size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+char *fgets(char *s, int size, FILE *stream);
 
 int usleep(int usecs);
 
