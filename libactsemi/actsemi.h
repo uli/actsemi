44d6af1dca5ebaedd7e5ed5f1d416bd00f788f29
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-12-20 15:47:05 +0100

    libactsemi: key structure and constants

--- libactsemi/actsemi.h
+++ libactsemi/actsemi.h
@@ -22,4 +22,20 @@ int dlclose(void *handle);
 unsigned int get_count(void);
 int clock_gettime(int clk, uint32_t *time);
 
+#define EMU_KEY_START 3
+#define EMU_KEY_SELECT 4
+#define EMU_KEY_LEFT 5
+#define EMU_KEY_RIGHT 6
+#define EMU_KEY_UP 7
+#define EMU_KEY_DOWN 8
+#define EMU_KEY_L 9
+#define EMU_KEY_R 10
+#define EMU_KEY_SQUARE 17
+#define EMU_KEY_CROSS 18
+#define EMU_KEY_CIRCLE 19
+#define EMU_KEY_TRIANGLE 20
 
+typedef struct {
+    uint8_t p1[0x16];
+    uint8_t p2[0x16];
+} emu_keys_t;
